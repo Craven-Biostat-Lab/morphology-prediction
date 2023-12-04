a50a1be7eda8c03a6ff9bd041822a4aae08dfe78
Yuriy Sverchkov <yuriy@biostat.wisc.edu>
2023-12-04 14:33:28 -0600

    tracking message edit

--- gnn/train_nnc.py
+++ gnn/train_nnc.py
@@ -167,7 +167,7 @@ def training_loop(data, model, epochs=200):
                     value = compute(y_hat[mask, 0], y[mask, 0]).numpy(force=True)
                     loss_curves[(subset, metric)][epoch] = value
                     if epoch % 10 == 9:
-                        print(f"epoch {epoch}: {subset} {mask} = {value}")
+                        print(f"epoch {epoch}: {subset} {metric} = {value}")
 
         # Save best model
         auroc = loss_curves[('validation', 'auroc')][epoch]
