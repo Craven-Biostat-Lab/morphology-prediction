ad3f182e972996b07138b5886240504b616eb1d7
Yuriy Sverchkov <yuriy@biostat.wisc.edu>
2023-12-06 14:07:01 -0600

    Split out net definition to separate file

---
+++ gnn/nets.py
@@ -0,0 +1,89 @@
+"""Network architectures are collected here"""
+
+import torch
+import torch.nn.functional as func
+from torch_geometric.nn import NNConv
+
+class NNConvNet(torch.nn.Module):
+    """Our application of NNConv"""
+
+    def __init__(
+        self,
+        in_features,
+        edge_features,
+        channel_width,
+        encoder_depth=0,
+        gnn_depth=1,
+        decoder_depth=0,
+        out_features=1,
+        dropout_rate=0.5
+    ):
+        super().__init__()
+
+        # Record network depth
+        self.dropout_rate = dropout_rate
+
+        # First layer: input x to channel width
+        self.lin_in = torch.nn.Linear(in_features, channel_width, bias=True)
+
+        # Encoder layers
+        self.encoder = torch.nn.Sequential(
+            torch.nn.Sequential(
+                torch.nn.LeakyReLU(),
+                torch.nn.Dropout(p=dropout_rate),
+                torch.nn.Linear(channel_width, channel_width, bias=True)
+            )
+            for _ in range(encoder_depth)
+        )
+
+        # Middle layers: NNConv layers
+        # NN for NNConv is shared across layers
+        self.edge_nn = torch.nn.Sequential(
+            torch.nn.Linear(edge_features, channel_width**2, bias=True),
+            torch.nn.LeakyReLU()
+        )
+
+        # The NNConv layers
+        self.nnconv = torch.nn.ModuleList(
+            NNConv(
+                in_channels=channel_width,
+                out_channels=channel_width,
+                nn=self.edge_nn,
+                aggr='add',
+                root_weight=True,
+                bias=True
+            )
+            for _ in range(gnn_depth)
+        )
+
+        # Decoder layers
+        self.decoder = torch.nn.Sequential(
+            torch.nn.Sequential(
+                torch.nn.LeakyReLU(),
+                torch.nn.Dropout(p=dropout_rate),
+                torch.nn.Linear(channel_width, channel_width, bias=True)
+            )
+            for _ in range(decoder_depth)
+        )
+
+        # Last layer: transform to result dimension.
+        self.lin_out = torch.nn.Linear(channel_width, out_features, bias=True)
+    
+    def forward(self, data):
+        x, edge_index, edge_attr = data.x, data.edge_index, data.edge_attr
+
+        h = self.lin_in(x)
+        h = self.encoder(h)
+
+        for nnc_layer in self.nnconv:
+            h = func.leaky_relu(h)
+            h = func.dropout(h, p=self.dropout_rate)
+            h = nnc_layer(h, edge_index, edge_attr)
+
+        h = self.decoder(h)
+
+        out = torch.squeeze(self.lin_out(h), -1)
+
+        return func.log_softmax(
+            torch.stack([torch.zeros_like(out), out], dim=1),
+            dim=1)
--- gnn/train_nnc.py
+++ gnn/train_nnc.py
@@ -9,9 +9,9 @@ import pandas as pd
 import torch
 import torch.nn.functional as func
 
-from torch_geometric.nn import NNConv
-
 from torcheval.metrics.functional import binary_auroc, binary_normalized_entropy
+
+from nets import NNConvNet
 
 # May use ignite in the future
 #from ignite.engine import Engine, Events
@@ -44,95 +44,6 @@ def create_parser():
 
     return parser
 
-
-def log_expit(x):
-    """Implements log ( 1 / ( 1 + exp( -x ) ) )"""
-    stack = torch.stack([x, torch.zeros_like(x)], dim=0)
-    return torch.nn.functional.log_softmax(stack, dim=0)[0]
-
-
-class NNConvNet(torch.nn.Module):
-    """Our application of NNConv"""
-
-    def __init__(
-        self,
-        in_features,
-        edge_features,
-        channel_width,
-        encoder_depth=0,
-        gnn_depth=1,
-        decoder_depth=0,
-        out_features=1,
-        dropout_rate=0.5
-    ):
-        super().__init__()
-
-        # Record network depth
-        self.dropout_rate = dropout_rate
-
-        # First layer: input x to channel width
-        self.lin_in = torch.nn.Linear(in_features, channel_width, bias=True)
-
-        # Encoder layers
-        self.encoder = torch.nn.ModuleList(
-            torch.nn.Linear(channel_width, channel_width, bias=True)
-            for _ in range(encoder_depth)
-        )
-
-        # Middle layers: NNConv layers
-        # NN for NNConv is shared across layers
-        self.edge_nn = torch.nn.Sequential(
-            torch.nn.Linear(edge_features, channel_width**2, bias=True),
-            torch.nn.LeakyReLU()
-        )
-
-        # The NNConv layers
-        self.nnconv = torch.nn.ModuleList(
-            NNConv(
-                in_channels=channel_width,
-                out_channels=channel_width,
-                nn=self.edge_nn,
-                aggr='add',
-                root_weight=True,
-                bias=True
-            )
-            for _ in range(gnn_depth)
-        )
-
-        # Decoder layers
-        self.decoder = torch.nn.ModuleList(
-            torch.nn.Linear(channel_width, channel_width, bias=True)
-            for _ in range(decoder_depth)
-        )
-
-        # Last layer: transform to result dimension.
-        self.lin_out = torch.nn.Linear(channel_width, out_features, bias=True)
-    
-    def forward(self, data):
-        x, edge_index, edge_attr = data.x, data.edge_index, data.edge_attr
-
-        h = self.lin_in(x)
-
-        for encoder_layer in self.encoder:
-            h = func.leaky_relu(h)
-            h = func.dropout(h, p=self.dropout_rate)
-            h = encoder_layer(h)
-
-        for nnc_layer in self.nnconv:
-            h = func.leaky_relu(h)
-            h = func.dropout(h, p=self.dropout_rate)
-            h = nnc_layer(h, edge_index, edge_attr)
-
-        for decoder_layer in self.decoder:
-            h = func.leaky_relu(h)
-            h = func.dropout(h, p=self.dropout_rate)
-            h = decoder_layer(h)
-
-        out = torch.squeeze(self.lin_out(h), -1)
-
-        return torch.nn.functional.log_softmax(
-            torch.stack([out, torch.zeros_like(out)], dim=1),
-            dim=1)
 
 def training_loop(data, model, epochs=200):
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
