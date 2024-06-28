e7e459cad3fbafa61194698d6aa92a778f99885e
Yuriy Sverchkov <yuriy@biostat.wisc.edu>
2024-06-28 11:47:22 -0500

    Added morphology data loader

--- skl-models/dataloader.py
+++ skl-models/dataloader.py
@@ -1,4 +1,8 @@
+import logging
+from pathlib import Path
 import pandas as pd
+
+logger = logging.getLogger(__name__)
 
 class FunctionalData():
 
@@ -64,3 +68,33 @@ class FunctionalData():
             'Abundance+SC': list(abundance_df.columns)+list(sc_df.columns),
             'All': self.gene_features.columns
         }
+
+
+class MorphologyData:
+
+    def __init__(
+        self,
+        data_path,
+        index_cols=['Metadata_Plate', 'Metadata_Well'],
+        metadata_prefix='Metadata_',
+        gene_col='Metadata_Symbol',
+        file_format=None # Inferred from filename
+    ) -> None:
+        
+        if file_format is None:
+            file_format = Path(data_path).suffix.lstrip('.')
+            logger.info("Inferred file format '{file_format}' from file extension.")
+        
+        if file_format == 'csv':
+            self.normalized_profiles = pd.read_csv(data_path, index_col=index_cols)
+        elif file_format == 'parquet':
+            self.normalized_profiles = pd.read_parquet(data_path).set_index(index_cols)
+        else:
+            msg = f"I don't know how to read a '{file_format}' file"
+            logger.error(msg)
+            raise ValueError(msg)
+        
+        all_columns = self.normalized_profiles.columns
+        self.profile_columns = all_columns[~all_columns.str.startswith(metadata_prefix)]
+
+        self.gene_profiles = self.normalized_profiles.groupby(gene_col)[self.profile_columns].mean()
