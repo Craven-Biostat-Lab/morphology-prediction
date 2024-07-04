9f54653a2e2632c7797eaacdb88f1aaf92a6a0fb
Yuriy Sverchkov <sverchkov@wisc.edu>
2024-07-03 20:10:47 -0500

    Working version of downloader

--- cpg_downloader.py
+++ cpg_downloader.py
@@ -2,6 +2,7 @@
 
 from argparse import ArgumentParser
 import logging
+from pathlib import Path
 
 import numpy as np
 import pandas as pd
@@ -10,7 +11,7 @@ import pycytominer
 logger = logging.getLogger(__name__)
 
 DEFAULT_CONFIG = {
-    'log level': logging.DEBUG,
+    'log level': logging.INFO,
     'dataset': 'cpg0003-A549',
     'cpg0003 uri template': (
         "s3://cellpainting-gallery/cpg0003-rosetta/"
@@ -29,7 +30,7 @@ KNOWN_DATASETS = {
             "LUAD-BBBC041-Caicedo/CellPainting/"
             "replicate_level_cp_augmented.csv.gz"
         ),
-        'add to metadata cols': ['Symbol']
+        'rename cols': {'Symbol': 'Metadata_Symbol'}
     },
     'cpg0003-U2OS': {
         'dataset uri': (
@@ -37,21 +38,34 @@ KNOWN_DATASETS = {
             "broad/workspace/preprocessed_data/"
             "TA-ORF-BBBC037-Rohban/CellPainting/"
             "replicate_level_cp_augmented.csv.gz"
-        )
-    },
+        ),
+        'rename cols': {
+            'Metadata_GeneID': 'Metadata_NCBIGeneID',
+            'Metadata_gene_name': 'Metadata_Symbol',
+            'pert_type': 'Metadata_pert_type'
+        }
+    }
     #'cpg0016': None
 }
 
 
 def create_parser() -> ArgumentParser:
-    parser = ArgumentParser()
+    parser = ArgumentParser(description='Download control-normalized CPG data.')
+    parser.add_argument('-d', '--dataset', choices=KNOWN_DATASETS.keys(), required=True)
+    parser.add_argument('-o', '--output', type=Path, required=True)
+    parser.add_argument('-v', '--verbose', action = 'store_true')
     return parser
 
 
 def get_config(args):
     config = {} | DEFAULT_CONFIG
+    config['log level'] = logging.DEBUG if args.verbose else logging.INFO
+    if args.dataset:
+        config['dataset'] = args.dataset
+    if args.output:
+        config['out_path'] = args.output
     if not config.get('dataset uri'):
-        config['dataset uri'].update(KNOWN_DATASETS[config['dataset']])
+        config.update(KNOWN_DATASETS[config['dataset']])
     return config
 
 
@@ -60,8 +74,7 @@ def main(config):
     # Load profiles
     profiles = pd.read_csv(
         config['dataset uri'],
-        storage_options={'anon': True},
-        dtype=config['explicit dtypes']
+        storage_options={'anon': True}
     )
 
     columns = profiles.columns
@@ -74,15 +87,23 @@ def main(config):
     ]
 
     # Rename columns if needed
-    columns_to_add = config.get('add to metadata cols')
-    if columns_to_add:
-        profiles.rename({c: f'Metadata_{c}' for c in columns_to_add}, inplace=True)
+    columns_to_rename = config.get('rename cols')
+    if columns_to_rename:
+        logger.info(f'Renaming columns according to {columns_to_rename}.')
+        # Drop renaming targets if they are present (to avoid duplicate column names downstream)
+        profiles.drop(columns=columns_to_rename.values(), inplace=True, errors='ignore')
+        # Rename columns
+        profiles.rename(columns=columns_to_rename, inplace=True)
         columns = profiles.columns
+        logger.debug(f'Renaming result: {columns}')
+    
+    # Clean up Metadata_NCBIGeneID
+    profiles['Metadata_NCBIGeneID'] = profiles['Metadata_NCBIGeneID'].astype('Int64').astype('str')
 
-    # Identify metadata columns
-    meta_cols = columns[columns.str.startswith('Metadata_')]
+    # Identify indexing columns
+    meta_cols = ['Metadata_Plate', 'Metadata_Well', 'Metadata_NCBIGeneID', 'Metadata_Symbol', 'Metadata_pert_type']
 
-    # Identify controls
+    logger.debug(profiles[meta_cols])
 
     # Normalize
     normalized_profiles = profiles.set_index(meta_cols).groupby('Metadata_Plate', group_keys=False).apply(
